c9c1da901d930efb9e056a8602cdd0d45d8965be
JPMillward <78622369+JPMillward@users.noreply.github.com>
2021-03-05 01:51:55 -0500

    d

--- motd_bot.py
+++ motd_bot.py
@@ -1,13 +1,18 @@
 import discord
 import asyncio
 import os
-from os.path import join, dirname
 from dotenv import load_dotenv
 
 load_dotenv()
-
 DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
 REQUEST_LINK = os.environ.get("REQUEST_LINK")
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+client.run(DISCORD_TOKEN)
 
 print(DISCORD_TOKEN)
 print(REQUEST_LINK)
