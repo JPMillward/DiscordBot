51283603335743bbda8efd60eb2efecc5ada2bec
lvgvt <lvg1159@gmail.com>
2021-03-05 01:00:24 -0500

    udpated motd file

--- motd_bot.py
+++ motd_bot.py
@@ -1,27 +1,11 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar  4 22:08:45 2021
-
-@author: johnm
-"""
-
-import discord
-from motd_class import Motd
-from dotenv import load_dotenv, find_dotenv
 import os
 from os.path import join, dirname
+from dotenv import load_dotenv
 
+load_dotenv()
 
-#Environment Variables for Discord Bot
-#DISCORD_TOKEN=ODE0MjMxOTcyODA0ODIxMDAy.YDa2ng.wmRMKLmo7xw7rzAgkf5nn4dEZAQ
-#REQUEST_LINK=https://www.endofthelinegaming.com/wp-json/wp/v2/posts?per_page=1&
-#https://www.endofthelinegaming.com/wp-json/wp/v2/posts?per_page=1&\
-dotenv_path = join(dirname("~/Documents/GitHub/DiscordBot/"), '.env')
-load_dotenv(dotenv_path)
-TOKEN = os.getenv("DISCORD_TOKEN")
-EOTL_API = os.getenv("REQUEST_LINK")
-print(TOKEN)
-print(str(TOKEN))
-print(repr(TOKEN))
-print(repr(EOTL_API))
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+REQUEST_LINK = os.environ.get("REQUEST_LINK")
+
+print(DISCORD_TOKEN)
+print(REQUEST_LINK)
